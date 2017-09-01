b6bcbf65a00481d4c8e2c876d9774687d2b72ac4
Jesse <wsm369@126.com>
2017-09-01 12:45:39 +0800

    new

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * 我的第一个应用程序
+ * @author Administrator
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");//输出hello world;
+    }
+}
